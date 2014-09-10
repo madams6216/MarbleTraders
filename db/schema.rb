e706c3519a361d56e3663b9c10d87c3b87eabe78
msimcoe <msimcoe@COMcast.net>
2014-09-10 19:51:16 -0400

    added numerous style changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904135926) do
+ActiveRecord::Schema.define(version: 20140909170604) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20140904135926) do
     t.text     "marble_text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "models", force: true do |t|
