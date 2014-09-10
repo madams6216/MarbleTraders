c9349a066c90e258b6baba55d913d20c9b0e2173
samskinner <samskinnerphd@gmail.com>
2014-09-10 08:51:17 -0400

    security key changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   :storage => :s3,
   :s3_credentials => {
     :bucket => 'marbletrader',   #add the appropriate data to green parts
-    :access_key_id => 'AKIAJWCZCCD6JQQWROTA',
-    :secret_access_key => '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'
+    :access_key_id => 'AKIAIKVN6JU76DNXPMDQ',
+    :secret_access_key => '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'
   }
 }
 end
